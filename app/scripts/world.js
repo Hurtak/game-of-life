b148e6791a2f2ac0c3facb12f4f5b8b11e333225
hurtak <petr.hurtak@gmail.com>
2015-12-30 22:04:54 +0100

    world: add world tick logic

--- app/scripts/world.js
+++ app/scripts/world.js
@@ -1,8 +1,63 @@
-export const addCell = (world, x, y) => {
-	const cellExists = world.find(item => item.x === x && item.y === y);
+const getCell = (world, x, y) => {
+	return world.find(item => item.x === x && item.y === y) || false;
+};
+
+const getNeighboursCoordinates = (x, y) => {
+	return [
+		{x: x+1, y: y  },
+		{x: x  , y: y+1},
+		{x: x+1, y: y+1},
+
+		{x: x-1, y: y  },
+		{x: x  , y: y-1},
+		{x: x-1, y: y-1},
+
+		{x: x+1, y: y-1},
+		{x: x-1, y: y+1}
+	];
+};
+
+const getNeighbourCount = (world, x, y) => {
+	const neighbours = getNeighboursCoordinates(x, y);
+	return neighbours.reduce((p, c) => p + (getCell(world, c.x, c.y) ? 1 : 0), 0);
+};
+
+const addCell = (world, x, y) => {
+	const cellExists = getCell(world, x, y);
 	return cellExists ? world : [...world, {x, y}];
 };
 
-export const removeCell = (world, x, y) => {
+const removeCell = (world, x, y) => {
 	return world.filter(cell => !(cell.x === x && cell.y === y));
 };
+
+const tick = (world) => {
+	let newWorld = [];
+	let neighbours = [];
+
+	world.forEach(cell => {
+		const neighbourCount = getNeighbourCount(world, cell.x, cell.y);
+
+		const neighbourCoordinates = getNeighboursCoordinates(cell.x, cell.y);
+		neighbourCoordinates.forEach(neighbour => {
+			neighbours = addCell(neighbours, neighbour.x, neighbour.y)
+		});
+
+		// keep cells with 2-3 neighbourCount
+		if (neighbourCount === 2 || neighbourCount === 3) {
+			newWorld = addCell(newWorld, cell.x, cell.y);
+		}
+	});
+
+	// check neighbours of alive cells to see if they have 3 living neighbours, in which case they should come alive
+	neighbours.forEach(cell => {
+		const neighbourCount = getNeighbourCount(world, cell.x, cell.y);
+		if (neighbourCount === 3) {
+			newWorld = addCell(newWorld, cell.x, cell.y);
+		}
+	});
+
+	return newWorld;
+};
+
+export {addCell, removeCell, tick}
--- test/world.test.js
+++ test/world.test.js
@@ -3,42 +3,34 @@ import 'babel-core/register';
 
 import * as game from '../app/scripts/world.js';
 
-test('add cell into empty world', t => {
+test('add cell', t => {
 	t.same(
 		game.addCell([], 0, 0),
 		[{x: 0, y: 0}]
 	);
-});
 
-test('add cell which already exists', t => {
 	t.same(
 		game.addCell([{x: 0, y: 0}], 0, 0),
 		[{x: 0, y: 0}]
 	);
-});
 
-test('add cell to non empty world', t => {
 	t.same(
 		game.addCell([{x: 0, y: 0}], 1, 1),
 		[{x: 0, y: 0}, {x: 1, y: 1}]
 	);
 });
 
-test('remove cell from empty world', t => {
+test('remove cell', t => {
 	t.same(
 		game.removeCell([], 0, 0),
 		[]
 	);
-});
 
-test('remove cell non existent cell', t => {
 	t.same(
 		game.removeCell([{x: 0, y: 0}], 1, 1),
 		[{x: 0, y: 0}]
 	);
-});
 
-test('remove cell existing cell', t => {
 	t.same(
 		game.removeCell([{x: 0, y: 0}], 0, 0),
 		[]
@@ -58,4 +50,31 @@ test('remove cell existing cell', t => {
 	];
 
 	t.same(game.removeCell(before, 1, 0), after);
+});
+
+test('world tick', t => {
+	t.same(
+		game.tick([{x: 0, y: 0}]),
+		[]
+	);
+
+	t.same(
+		game.tick([{x: 0, y: 0}, {x: 0, y: 1}]),
+		[]
+	);
+
+	t.same(
+		game.tick([{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}]),
+		[{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}]
+	);
+
+	t.same(
+		game.tick([{x: 0, y: 0}, {x: 1, y: 0}, {x: 2, y: 0}]),
+		[{x: 1, y: 0}, {x: 1, y: 1}, {x: 1, y: -1}]
+	);
+
+	t.same(
+		game.tick([{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}]),
+		[{x: 0, y: 0}, {x: 0, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}]
+	);
 });
