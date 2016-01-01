823e1a7ea062c3478fb1bd6a1fd4dad541d6c73b
hurtak <petr.hurtak@gmail.com>
2016-01-01 22:17:17 +0100

    canvas: reuse canvas contect to improve performance

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -1,45 +1,41 @@
-const MAX_CELLS_X = 50
-const MAX_CELLS_Y = 50
+const CELLS_X = 50
+const CELLS_Y = 50
 const COLOR = '#000'
 
-let canvasEl
 let canvasSize = {width: 0, height: 0}
+let canvascontext
 
 const init = (canvasId) => {
-  canvasEl = document.getElementById(canvasId)
+  const canvasEl = document.getElementById(canvasId)
   canvasSize = {
     width: canvasEl.clientWidth,
     height: canvasEl.clientHeight
   }
 
+  canvascontext = canvasEl.getContext('2d')
+  canvascontext.fillStyle = COLOR
+
   canvasEl.addEventListener('mousedown', mousedown)
-  draw(0, 0)
-  draw(1, 1)
-  draw(2, 0)
 }
 
-const draw = (x, y) => {
-  drawCell(canvasEl, canvasSize, COLOR, MAX_CELLS_X, MAX_CELLS_Y, x, y)
+const drawCell = (x, y) => {
+  drawRect(canvascontext, canvasSize.width, canvasSize.height, CELLS_X, CELLS_Y, x, y)
 }
 
-const drawCell = (canvasEl, canvasSize, color, maxCellsX, maxCellsY, x, y) => {
-  const context = canvasEl.getContext('2d')
+const drawRect = (context, canvasWidth, canvasHeight, cellsX, cellsY, x, y) => {
+  const rectWidth = canvasWidth / cellsX
+  const rectHeight = canvasHeight / cellsY
+  const rectX = rectWidth * x
+  const rectY = canvasHeight - rectHeight * y - rectHeight
 
-  const drawWidth = canvasSize.width / maxCellsX
-  const drawHeight = canvasSize.height / maxCellsY
-  const drawX = drawWidth * x
-  const drawY = canvasSize.height - drawHeight * y - drawHeight
-
-  context.fillStyle = COLOR
-  context.fillRect(drawX, drawY, drawWidth, drawHeight)
+  context.fillRect(rectX, rectY, rectWidth, rectHeight)
 }
 
 const clear = () => {
-  clearCanvas(canvasEl, canvasSize.width, canvasSize.height)
+  clearCanvas(canvascontext, canvasSize.width, canvasSize.height)
 }
 
-const clearCanvas = (canvasEl, width, height) => {
-  const context = canvasEl.getContext('2d')
+const clearCanvas = (context, width, height) => {
   context.clearRect(0, 0, width, height)
 }
 
