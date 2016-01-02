9fb9e95e244827c7e1ab0393ef5a6cbbb14c3862
hurtak <petr.hurtak@gmail.com>
2016-01-02 12:52:30 +0100

    canvas: refactor some functions into pure ones and put them into utils

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -1,57 +1,43 @@
+import { drawRect, clearCanvas, canvasClick } from './utils/canvas.js'
+
+// Config
+
 const CELLS_X = 20
 const CELLS_Y = 20
 const COLOR = '#000'
 
-let canvasSize = {width: 0, height: 0}
-let canvascontext
+// Main functions
 
 const init = (canvasId) => {
   const canvasEl = document.getElementById(canvasId)
-  canvasSize = {
+  const canvasSize = {
     width: canvasEl.clientWidth,
     height: canvasEl.clientHeight
   }
 
-  canvascontext = canvasEl.getContext('2d')
+  let canvascontext = canvasEl.getContext('2d')
   canvascontext.fillStyle = COLOR
+
+  const drawCell = (x, y) => {
+    drawRect(canvascontext, canvasSize.width, canvasSize.height, CELLS_X, CELLS_Y, x, y)
+  }
+
+  const clear = () => {
+    clearCanvas(canvascontext, canvasSize.width, canvasSize.height)
+  }
 
   drawCell(0, 0)
   drawCell(1, 1)
   drawCell(2, 2)
   drawCell(19, 19)
 
-  canvasEl.addEventListener('mousedown', mousedown)
+  canvasEl.addEventListener('click', (e) => {
+    const cellCoordinates = canvasClick(e.offsetX, e.offsetY, canvasSize.width, canvasSize.height, CELLS_X, CELLS_Y)
+    const [x, y] = cellCoordinates
+    drawCell(x, y)
+  })
 }
 
-const drawCell = (x, y) => {
-  drawRect(canvascontext, canvasSize.width, canvasSize.height, CELLS_X, CELLS_Y, x, y)
-}
-
-const drawRect = (context, canvasWidth, canvasHeight, cellsX, cellsY, x, y) => {
-  const rectWidth = canvasWidth / cellsX
-  const rectHeight = canvasHeight / cellsY
-  const rectX = rectWidth * x
-  const rectY = canvasHeight - rectHeight * y - rectHeight
-
-  context.fillRect(rectX, rectY, rectWidth, rectHeight)
-}
-
-const clear = () => {
-  clearCanvas(canvascontext, canvasSize.width, canvasSize.height)
-}
-
-const clearCanvas = (context, width, height) => {
-  context.clearRect(0, 0, width, height)
-}
-
-const mousedown = (e) => {
-  const rectWidth = canvasSize.width / CELLS_X
-  const rectHeight = canvasSize.height / CELLS_Y
-
-  const cellX = Math.floor(e.offsetX / rectWidth)
-  const cellY = CELLS_Y - Math.floor(e.offsetY / rectHeight) - 1
-
-  console.log('cell ', [cellX, cellY])
-}
+// Export
 
 export { init }
---
+++ app/scripts/utils/canvas.js
@@ -0,0 +1,22 @@
+export const clearCanvas = (context, width, height) => {
+  context.clearRect(0, 0, width, height)
+}
+
+export const drawRect = (context, canvasWidth, canvasHeight, cellsX, cellsY, x, y) => {
+  const rectWidth = canvasWidth / cellsX
+  const rectHeight = canvasHeight / cellsY
+  const rectX = rectWidth * x
+  const rectY = canvasHeight - rectHeight * y - rectHeight
+
+  context.fillRect(rectX, rectY, rectWidth, rectHeight)
+}
+
+export const canvasClick = (clickX, clickY, canvasWidth, canvasHeight, cellsX, cellsY) => {
+  const rectWidth = canvasWidth / cellsX
+  const rectHeight = canvasHeight / cellsY
+
+  const cellX = Math.floor(clickX / rectWidth)
+  const cellY = cellsY - Math.floor(clickY / rectHeight) - 1
+
+  return [cellX, cellY]
+}
