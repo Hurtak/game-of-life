61e257664dcba7c9dea55d9775a91a5ec93b4d5f
Petr Huřťák <petr.hurtak@gmail.com>
2016-03-28 18:30:04 +0200

    new button style for timer toggle

--- app/scripts/modules/controls.js
+++ app/scripts/modules/controls.js
@@ -9,10 +9,7 @@ const conf = {
 const dom = {
   getButtonStepEl: () => document.getElementById('button-step'),
   getButtonClearEl: () => document.getElementById('button-clear'),
-  getButtonTimerToggleEl: () => document.getElementById('button-timer-toggle'),
-
-  getIntervalsWrapperEl: () => document.getElementById('button-timer-intervals'),
-  getButtonTimerIntervalEl: (value) => document.querySelector(`[${ conf.intervalButtonAttribute }="${ value }"`)
+  getButtonTimerToggleEl: () => document.getElementById('button-timer-toggle')
 }
 
 let previousState
@@ -24,48 +21,7 @@ const init = (store) => {
   previousState = store.getState()
 
   dom.getButtonStepEl().addEventListener('click', () => store.dispatch({type: 'TICK'}))
-  dom.getButtonTimerToggleEl().addEventListener('click', () => store.dispatch({type: 'TOGGLE_TIMER'}))
   dom.getButtonClearEl().addEventListener('click', () => store.dispatch({type: 'CLEAR_WORLD'}))
-
-  store.subscribe(() => { stateChangeHandler(store) })
-}
-
-const stateChangeHandler = (store) => {
-  const currentState = store.getState()
-  if (currentState.timer.enabled !== previousState.timer.enabled) {
-    timerStateChanged(dom, conf)
-    timer = currentState.timer.enabled ? startTimer(timer, store, currentState.timer.interval) : window.clearInterval(timer)
-  }
-
-  if (currentState.timer.interval !== previousState.timer.interval) {
-    if (currentState.timer.enabled) {
-      timer = window.clearInterval(timer)
-      timer = startTimer(timer, store, currentState.timer.interval)
-    }
-  }
-
-  previousState = currentState
-}
-
-// --- Pure functions ----------------------------------------------------------
-
-const timerStateChanged = (dom, conf) => {
-  const button = dom.getButtonTimerToggleEl()
-
-  toggleElementCaption(button, conf.toggleCaptionAttribute)
-  button.classList.toggle(conf.toggleButtonClass)
-}
-
-const startTimer = (timer, store, interval) => {
-  return window.setInterval(() => {
-    store.dispatch({type: 'TICK'})
-  }, interval)
-}
-
-const toggleElementCaption = (element, attributeName) => {
-  const currentCaption = element.innerHTML.trim()
-  element.innerHTML = element.getAttribute(attributeName)
-  element.setAttribute(attributeName, currentCaption)
 }
 
 // --- Export ------------------------------------------------------------------
--- app/scripts/modules/timer-interval.js
+++ app/scripts/modules/timer-interval.js
@@ -3,35 +3,56 @@ import slider from '../ui/slider.js'
 // --- Config & Local state ----------------------------------------------------
 
 const conf = {
-  intervals: [
-    0, // ms
-    10,
-    25,
-    50,
-    100,
-    250,
-    500,
-    1000,
-    2000
-  ],
+  intervals: [0, 10, 25, 50, 100, 250, 500, 1000, 2000], // ms
   initialIntervalIndex: 6
 }
 
 const dom = {
-  slider: document.getElementById('timer-intervals-slider')
+  intervalsSlider: document.getElementById('timer-intervals-slider'),
+  timerSwitch: document.getElementById('timer')
 }
+
+let previousState = {}
+let timer
 
 // --- Main methods ------------------------------------------------------------
 
 const init = (store) => {
   slider({
-    targetEl: dom.slider,
+    targetEl: dom.intervalsSlider,
     items: conf.intervals.map(x => x + 'ms'),
     initialIndex: conf.initialIntervalIndex,
     callback: (index) => {
       store.dispatch({ type: 'CHANGE_TIMER_INTERVAL', interval: conf.intervals[index] })
     }
   })
+
+  dom.timerSwitch.addEventListener('change', () => store.dispatch({type: 'TOGGLE_TIMER'}))
+  store.subscribe(() => stateChangeHandler(store))
+}
+
+const stateChangeHandler = (store) => {
+  const currentState = store.getState().timer
+  if (currentState.enabled !== previousState.enabled) {
+    timer = currentState.enabled
+      ? startTimer(store, currentState.interval)
+      : window.clearInterval(timer)
+  }
+
+  if (currentState.enabled && currentState.interval !== previousState.interval) {
+    timer = window.clearInterval(timer)
+    timer = startTimer(store, currentState.interval)
+  }
+
+  previousState = currentState
+}
+
+// --- Pure functions ----------------------------------------------------------
+
+const startTimer = (store, interval) => {
+  return window.setInterval(() => {
+    store.dispatch({type: 'TICK'})
+  }, interval)
 }
 
 // --- Export ------------------------------------------------------------------
