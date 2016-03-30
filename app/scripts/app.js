1672c0535453929298ee7687511505f310cb9a71
Petr Huřťák <petr.hurtak@gmail.com>
2016-03-30 17:49:18 +0200

    merge world-size.js with controls.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,15 +2,13 @@ import { createStore } from 'redux'
 import reducers from './reducers.js'
 
 import canvas from './modules/canvas.js'
-import controls from './modules/controls.js'
-import worldSize from './modules/world-size.js'
+import world from './modules/world-size.js'
 import timer from './modules/timer.js'
 import stats from './modules/stats.js'
 
 const store = createStore(reducers)
 
 canvas(store)
-worldSize(store)
+world(store)
 timer(store)
-controls(store)
 stats(store)
--- app/scripts/modules/world-size.js
+++ app/scripts/modules/world-size.js
@@ -4,20 +4,25 @@ import slider from '../ui/slider.js'
 // --- Config & Local state ----------------------------------------------------
 
 const dom = {
-  slider: document.getElementById('world-sizes-slider')
+  worldSizeSlider: document.getElementById('world-sizes-slider'),
+  worldTickButton: document.getElementById('button-step'),
+  worldClearButton: document.getElementById('button-clear')
 }
 
 // --- Main methods ------------------------------------------------------------
 
 const init = (store) => {
   slider({
-    targetEl: dom.slider,
+    targetEl: dom.worldSizeSlider,
     items: conf.world.dimensions.map(x => x.join(' &times; ')),
     initialIndex: conf.world.initialIndex,
     callback: (index) => {
       store.dispatch({ type: 'CHANGE_WORLD_SIZE', dimensions: conf.world.dimensions[index] })
     }
   })
+
+  dom.worldTickButton.addEventListener('click', () => store.dispatch({type: 'TICK'}))
+  dom.worldClearButton.addEventListener('click', () => store.dispatch({type: 'CLEAR_WORLD'}))
 }
 
 // --- Export ------------------------------------------------------------------
