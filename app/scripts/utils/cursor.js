644859f99d3893c4f9430c89534623902257f895
Petr Huřťák <petr.hurtak@firma.seznam.cz>
2016-03-31 17:45:45 +0200

    cursors are now defined as array of template literals, previously one newline separated literal

--- app/scripts/utils/cursor.js
+++ app/scripts/utils/cursor.js
@@ -1,54 +1,54 @@
 
-const cursors = `
+const cursors = [
+  `
   ■
-
+  `,
+  `
   ■ ■
   ■ ■
-
+  `,
+  `
     ■
   ■ ■ ■
     ■
-
+  `,
+  `
       ■
     ■ ■ ■
   ■ ■ ■ ■ ■
     ■ ■ ■
       ■
-
+  `,
+  `
     ■ ■
   ■ ■
     ■
-`
+  `
+]
 
 export default (string) => {
   console.time(1)
 
   const output = cursors
-    // 1. split string to lines
-    .split('\n')
-    // 2. group nonempty lines together
-    .reduce((patterns, line) => {
-      const newPattern = line.trim().length === 0
-      newPattern ? patterns.push([]) : patterns[patterns.length - 1].push(line)
-      return patterns
-    }, [])
-    // 3. filter empty cursors
-    .filter(array => array.length > 0)
-    // 4. remove trailing spaces from cursors
+    // 1. split cursor strings to lines
+    .map(cursors => cursors.split('\n'))
+    // 2. remove first and last line because they are empty
+    .map(cursor => cursor.slice(1, cursor.length - 1))
+    // 3. remove trailing spaces from cursors: ['  x', '  x x'] => ['x', 'x x']
     .map(cursor => {
-      // 4.1 determine shortest whitespace prefix
+      // 3.1 determine shortest whitespace prefix
       const cursorIndentation = cursor.reduce((shortestIndentation, line) => {
         const lineIndentation = line.match(/^\s+/)[0].length
         return lineIndentation < shortestIndentation ? lineIndentation : shortestIndentation
       }, Infinity)
-      // 4.2 remove this indentation
+      // 3.2 remove this indentation
       return cursor.map(line => line.replace(' '.repeat(cursorIndentation), ''))
     })
-    // 5. remove whitespace interlacing
+    // 4. remove whitespace interlacing: '  x x x' => ' xxx'
     .map(cursor => cursor.map(line => line.split('').filter((_, i) => i % 2 === 0).join('')))
-    // 6. convert strings to array of booleans: ' x ' => [false, true, false]
+    // 5. convert strings to array of booleans: ' x ' => [false, true, false]
     .map(cursor => cursor.map(line => line.split('').map(s => s !== ' ')))
-    // 7. convert to array of xy coordinates
+    // 6. convert to array of xy coordinates
     .map(cursor => {
       const coordinates = []
 
