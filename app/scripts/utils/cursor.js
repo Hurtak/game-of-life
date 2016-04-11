fe650b03cfe705aae5af603aa768a31df8d21462
Petr Huřťák <petr.hurtak@gmail.com>
2016-04-11 22:31:19 +0200

    rewrite cursor utils to take string instead of array of strings

--- app/scripts/utils/cursor.js
+++ app/scripts/utils/cursor.js
@@ -1,35 +1,50 @@
-export const cursorStringsToCoordinates = (arrayOfCursorStrings) => {
-  return arrayOfCursorStrings
-    // 1. split cursor strings to lines
-    .map(cursors => cursors.split('\n'))
-    // 2. remove first and last line because they are empty
-    .map(cursor => cursor.slice(1, cursor.length - 1))
-    // 3. remove trailing spaces from cursors: ['  x', '  x x'] => ['x', 'x x']
-    .map(cursor => {
-      // 3.1 determine shortest whitespace prefix
-      const cursorIndentation = cursor.reduce((shortestIndentation, line) => {
-        if (!line || line.match(/^\s$/)) return shortestIndentation
-        const currentIndentation = line.match(/^\s+/)[0].length
-        return currentIndentation < shortestIndentation ? currentIndentation : shortestIndentation
-      }, Infinity)
-      // 3.2 remove this indentation
-      return cursor.map(line => line.replace(' '.repeat(cursorIndentation), ''))
-    })
-    // 4. remove whitespace interlacing: '  x x x' => ' xxx'
-    .map(cursor => cursor.map(line => line.split('').filter((_, i) => i % 2 === 0).join('')))
-    // 5. convert strings to array of booleans: ' x ' => [false, true, false]
-    .map(cursor => cursor.map(line => line.split('').map(s => s !== ' ')))
-    // 6. convert to array of xy coordinates
-    .map(cursor => {
-      const coordinates = []
+export const cursorStringToCoordinates = (cursorString) => {
+  // 1. split cursor strings to lines
+  let lines = cursorString.split('\n')
 
-      for (let y = 0; y < cursor.length; y++) {
-        const line = cursor[y]
-        for (let x = 0; x < line.length; x++) {
-          if (!line[x]) continue
-          coordinates.push([x, cursor.length - y - 1])
-        }
-      }
-      return coordinates
-    })
+  // 2. remove first and last line because they are empty
+  const firstNonEmptyLine = (lines) => {
+    let currentIndex = 0
+    for (const line of lines) {
+      if (line.trim() !== '') return currentIndex
+      currentIndex++
+    }
+    return currentIndex
+  }
+
+  const startIndex = firstNonEmptyLine(lines)
+  const endIndex = firstNonEmptyLine([...lines].reverse())
+
+  lines = lines.slice(startIndex, lines.length - endIndex)
+
+  // 3. remove trailing spaces from cursors: ['  x', '  x x'] => ['x', 'x x']
+
+  // 3.1 determine shortest whitespace prefix
+  const cursorIndentation = lines.reduce((shortestIndentation, line) => {
+    if (!line || line.match(/^\s$/)) return shortestIndentation
+    const match = line.match(/^\s+/)
+    const currentIndentation = match ? match[0].length : 0
+    return currentIndentation < shortestIndentation ? currentIndentation : shortestIndentation
+  }, Infinity)
+
+  lines = lines
+    // 3.2 remove this indentation
+    .map(line => line.replace(' '.repeat(cursorIndentation), ''))
+    // 4. remove whitespace interlacing: '  x x x' => ' xxx'
+    .map(line => line.split('').filter((_, i) => i % 2 === 0).join(''))
+    // 5. convert strings to array of booleans: ' x ' => [false, true, false]
+    .map(line => line.split('').map(s => s !== ' '))
+
+  // 6. convert to array of xy coordinates
+  const coordinates = []
+
+  for (let y = 0; y < lines.length; y++) {
+    const line = lines[y]
+    for (let x = 0; x < line.length; x++) {
+      if (!line[x]) continue
+      coordinates.push([x, lines.length - y - 1])
+    }
+  }
+
+  return coordinates
 }
